c390ffef944427398d90557c0ec2cac5e73f6993
Thuong-Hai Pham <phamthuonghai@gmail.com>
2017-12-08 00:31:52 +0100

    dep_raw

--- t2t-dep/__init__.py
+++ t2t-dep/__init__.py
@@ -1,1 +1,1 @@
-from . import encs_small_problem, encs_dep_emb_small_problem, transformer_dep_emb
+from . import encs_small_problem, encs_dep_emb_small_problem, transformer_dep_emb, encs_dep_raw_small_problem, transformer_dep_raw
