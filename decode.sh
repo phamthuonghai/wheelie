c9078b19fb16d0bdffeacf4440798240f93a8196
Thuong-Hai Pham <phamthuonghai@gmail.com>
2017-12-18 02:20:53 +0100

    DepSubwordTextEncoder.decode()

--- decode.sh
+++ decode.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PROBLEM=translate_csen_dep_noid
-MODEL=transformer
+PROBLEM=translate_csen_dep_raw
+MODEL=transformer_dep_lrn
 HPARAMS=transformer_base_single_gpu
 
 HOME=`pwd`
-REF_FILE=$HOME/data/00test.ref
-DECODE_FILE=$HOME/data/00test.src
+REF_FILE=$HOME/data/plaintest_small.ref
+DECODE_FILE=$HOME/data/deptest_small.src
 TMP_DIR=$HOME/data/tmp
 DATA_DIR=$HOME/data/$PROBLEM
 TRAIN_DIR=$HOME/train_data/$PROBLEM/$MODEL-$HPARAMS
@@ -19,7 +19,7 @@ t2t-decoder \
   --hparams_set=$HPARAMS \
   --output_dir=$TRAIN_DIR \
   --t2t_usr_dir=$USR_DIR \
-  --decode_hparams='use_last_position_only=True' \
+  --decode_hparams='use_last_position_only=True,batch_size=8' \
   --decode_from_file=$DECODE_FILE
 
 sed -i '/^$/d' $DECODE_FILE.$MODEL.$HPARAMS.$PROBLEM.beam4.alpha0.6.decodes
