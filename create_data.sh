f63394a90643709cc94419a81477c32c17d21893
Thuong-Hai Pham <phamthuonghai@gmail.com>
2018-04-17 23:21:58 +0200

    create data without checking

--- create_data.sh
+++ create_data.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#$ -q gpu.q@dll[1256]
+#$ -l gpu=1,gpu_cc_min6.1=1,gpu_ram=8G
 #$ -N create_data
 #$ -cwd
 #$ -j y
@@ -8,9 +10,6 @@ PROBLEM=translate_csen_czeng
 
 HOME=$(pwd)
 
-deactivate
-source $HOME/.cpu-env/bin/activate
-
 TMP_DIR=$HOME/data/tmp
 DATA_DIR=$HOME/data/${PROBLEM}
 USR_DIR=$HOME/t2t-str
@@ -18,11 +17,9 @@ USR_DIR=$HOME/t2t-str
 mkdir -p ${TMP_DIR}
 
 # Generate data
-if [ ! -d "${DATA_DIR}" ]; then
-  mkdir -p ${DATA_DIR}
-  t2t-datagen \
-    --data_dir=${DATA_DIR} \
-    --tmp_dir=${TMP_DIR} \
-    --problem=${PROBLEM} \
-    --t2t_usr_dir=${USR_DIR}
-fi
+mkdir -p ${DATA_DIR}
+t2t-datagen \
+--data_dir=${DATA_DIR} \
+--tmp_dir=${TMP_DIR} \
+--problem=${PROBLEM} \
+--t2t_usr_dir=${USR_DIR}
